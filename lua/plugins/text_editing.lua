51b4bb88ba6922b7ec5a0825d697f8b9e46849af
JAVV <vinas.johnandrei@gmail.com>
2025-08-18 21:25:35 +0800

    moved comment.nvim block below

--- lua/plugins/text_editing.lua
+++ lua/plugins/text_editing.lua
@@ -1,6 +1,5 @@
 return {
    { 'echasnovski/mini.ai', version = '*', opts = {} },
-   { 'numToStr/Comment.nvim', opts = {} },
    { 'echasnovski/mini.move', version = '*', opts = {} },
    { 'echasnovski/mini.pairs', version = '*', opts = {} },
    { 'echasnovski/mini.splitjoin', version = '*', opts = {} },
