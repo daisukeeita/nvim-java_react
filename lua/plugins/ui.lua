e002af1d7788998f8d9f10074b8af4422bd1ad0c
JAVV <vinas.johnandrei@gmail.com>
2025-08-19 22:55:06 +0800

    added comments to separate plugins

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,63 +1,75 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    event = "VeryLazy",
-    config = function()
-      require("bufferline").setup( {
-        highlights = require("catppuccin.groups.integrations.bufferline").get{
-          styles = { "italic", "bold" }
-        }
-      } )
-    end
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    event = "VeryLazy",
-    config = function()
-      local lualine = require("lualine")
+	-- Buffers
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		event = "VeryLazy",
+		config = function()
+			require("bufferline").setup({
+				highlights = require("catppuccin.groups.integrations.bufferline").get({
+					styles = { "italic", "bold" },
+				}),
+			})
+		end,
+	},
 
-      local function lsp_attached()
-        local clients = vim.lsp.get_clients()
-        local buf_ft = vim.bo.filetype
-        local lsp_name = {}
+	-- Status Line
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		event = "VeryLazy",
+		config = function()
+			local lualine = require("lualine")
 
-        for _, client in pairs(clients) do
-          if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-            table.insert(lsp_names, client.name)
-          end
-        end
+			local function lsp_attached()
+				local clients = vim.lsp.get_clients()
+				local buf_ft = vim.bo.filetype
+				local lsp_names = {}
 
-        return #lsp_name > 0 and "  " .. table.concat(lsp_names, ", ") or "No LSP"
-      end
+				for _, client in pairs(clients) do
+					if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+						table.insert(lsp_names, client.name)
+					end
+				end
 
-      lualine.setup({
-        options = {
-          theme = "catppuccin",
-          section_separators = { left = '', right = '' },
-          component_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = { { 'mode', icon = '' } },
-          lualine_b = { 'branch', 'diff', 'diagnostics' },
-          lualine_c = { lsp_attached },
-          lualine_x = { 'encoding', 'fileformat', 'filetype' },
-          lualine_y = { 'progress' },
-          lualine_z = { { 'location', icon = '' } },
-        },
-        inactive_sessions = {
-          lualine_c = { 'filename' },
-          lualine_x = { 'location' },
-        }
-      })
-    end
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-  }
+				return #lsp_names > 0 and "  " .. table.concat(lsp_names, ", ") or "No LSP"
+			end
+
+			lualine.setup({
+				options = {
+					theme = "catppuccin",
+					section_separators = { left = "", right = "" },
+					component_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { { "mode", icon = "" } },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = { lsp_attached },
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { { "location", icon = "" } },
+				},
+				inactive_sessions = {
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+				},
+			})
+		end,
+	},
+
+	-- Indentation
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		---@module "ibl"
+		---@type ibl.config
+		opts = {},
+	},
+
+	-- Diagnostic UI
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+	},
 }
