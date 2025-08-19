951817aefee3a08b84fedb8ecb85b74b705907fd
JAVV <vinas.johnandrei@gmail.com>
2025-08-19 22:43:02 +0800

    added treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"java",
+					"javadoc",
+					"html",
+					"css",
+					"comment",
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
+}
