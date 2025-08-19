3dc83da7388b2f97beb5b9a7f8c87fd7fb8b3462
JAVV <vinas.johnandrei@gmail.com>
2025-08-19 22:54:17 +0800

    init

--- lua/plugins/dependencies.lua
+++ lua/plugins/dependencies.lua
@@ -1,5 +1,6 @@
 return {
-  { "nvim-lua/plenary.nvim" },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  { "nvim-tree/nvim-web-devicons", opts = {} }, 
+	{ "nvim-lua/plenary.nvim" },
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{ "nvim-neotest/nvim-nio" },
 }
