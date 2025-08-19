0d528e4bf80403f64f6b864bd0bbe9f5ac82ead6
JAVV <vinas.johnandrei@gmail.com>
2025-08-19 22:55:28 +0800

    import files

--- init.lua
+++ init.lua
@@ -3,4 +3,9 @@ require("core.lazy")
 
 -- Load Core Configurations
 require("core.options")
+require("core.diagnostics")
 require("core.keymaps")
+require("core.autocmd")
+
+-- Load LSP Configurations
+require("lsp.lua-lsp")
