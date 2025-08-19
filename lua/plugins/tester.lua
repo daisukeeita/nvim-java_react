c379671ecac6175fade4ce984cdcf1a5a5438eab
JAVV <vinas.johnandrei@gmail.com>
2025-08-19 22:42:55 +0800

    added tester and its adapters plugin

---
+++ lua/plugins/tester.lua
@@ -0,0 +1,27 @@
+return {
+	{
+		"rcasia/neotest-java",
+		ft = "java",
+		dependencies = {
+			-- "mfussenegger/nvim-jdtls",
+			"mfussenegger/nvim-dap", -- for the debugger
+			"rcarriga/nvim-dap-ui", -- recommended
+			"theHamsta/nvim-dap-virtual-text", -- recommended
+		},
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-java")({}),
+				},
+			})
+		end,
+	},
+}
