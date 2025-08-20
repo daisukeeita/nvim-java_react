e99b0f64bb3e818caf0116395d327678fb12c1b1
JAVV <vinas.johnandrei@gmail.com>
2025-08-20 08:51:36 +0800

    added integrations to plugin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,50 @@
 return {
-  {
-    "catppuccin/nvim", 
-    name = "catppuccin", 
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
-    opts = {
-      background = {
-        light = "latte",
-        dark = "macchiato"
-      },
-      transparent_background = false,
-    }
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("catppuccin")
+		end,
+		opts = {
+			background = {
+				light = "latte",
+				dark = "macchiato",
+			},
+			transparent_background = false,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				treesitter_context = true,
+				mini = {
+					enabled = true,
+				},
+				fidget = true,
+				fzf = true,
+				leap = true,
+				markdown = true,
+				mason = true,
+				neotest = true,
+				dap = true,
+				dap_ui = true,
+				nvim_surround = true,
+				telescope = {
+					enabled = true,
+				},
+				lsp_trouble = true,
+				which_key = true,
+			},
+		},
+	},
 
-  {
-    "folke/tokyonight.nvim",
-  },
+	{
+		"folke/tokyonight.nvim",
+	},
 
-  {
-    "morhetz/gruvbox",
-  },
-} 
+	{
+		"morhetz/gruvbox",
+	},
+}
