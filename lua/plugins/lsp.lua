8e5c02ddb145247455d35343933c3971a6f9cbaa
JAVV <vinas.johnandrei@gmail.com>
2025-08-20 08:52:05 +0800

    added nvim-jdtls plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,17 @@
 return {
-  { "neovim/nvim-lspconfig" },
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
-      }
-    }
-  },
-  { "j-hui/fidget.nvim", opts = {} },
+	{ "neovim/nvim-lspconfig" },
+	{
+		"mason-org/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{ "j-hui/fidget.nvim", opts = {} },
+	{ "mfussenegger/nvim-jdtls", ft = { "java" } },
 }
