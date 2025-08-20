6383784ae81bb751fc0a2dd6eefb98c434930139
JAVV <vinas.johnandrei@gmail.com>
2025-08-20 11:43:39 +0800

    added linter for Java

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -1,18 +1,19 @@
 return {
-  {
-    "mfussenegger/nvim-lint",
-    confing = function ()
-      require("lint").linters_by_ft = {
-        lua = { "luacheck" }
-      }
-    end
-  },
-  {
-    'stevearc/conform.nvim',
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-      }
-    },
-  }
+	{
+		"mfussenegger/nvim-lint",
+		confing = function()
+			require("lint").linters_by_ft = {
+				lua = { "luacheck" },
+				java = { "checkstyle" },
+			}
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		},
+	},
 }
