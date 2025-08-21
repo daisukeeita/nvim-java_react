24b54b61fe22dc66f5045eb73763586874e389c8
JAVV <vinas.johnandrei@gmail.com>
2025-08-21 13:23:53 +0800

    added ts-autotag plugin for JavaScript/TypeScript

--- lua/plugins/text_editing.lua
+++ lua/plugins/text_editing.lua
@@ -1,33 +1,34 @@
 return {
-   { 'echasnovski/mini.ai', version = '*', opts = {} },
-   { 'echasnovski/mini.move', version = '*', opts = {} },
-   { 'echasnovski/mini.pairs', version = '*', opts = {} },
-   { 'echasnovski/mini.surround', version = '*', opts = {} }, 
-   {
-     'echasnovski/mini.splitjoin',
-     version = '*',
-     opts = {
-       mappings = {
-         toggle = "<Leader>tsj"
-       }
-     }
-   },
-   {
-     'numToStr/Comment.nvim',
-     opts = {
-       toggler = {
-         line = "<Leader>tlc",
-         block = "<Leader>tbc"
-       },
-       opleader = {
-         line = "<Leader>tl",
-         block = "<Leader>tb",
-       },
-       extra = {
-         above = "<Leader>aca",
-         below = "<Leader>acb",
-         eol = "<Leader>ace",
-       },
-     }
-   },
+	{ "echasnovski/mini.ai", version = "*", opts = {} },
+	{ "echasnovski/mini.move", version = "*", opts = {} },
+	{ "echasnovski/mini.pairs", version = "*", opts = {} },
+	{ "echasnovski/mini.surround", version = "*", opts = {} },
+	{ "windwp/nvim-ts-autotag", opts = {} },
+	{
+		"echasnovski/mini.splitjoin",
+		version = "*",
+		opts = {
+			mappings = {
+				toggle = "<Leader>tsj",
+			},
+		},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			toggler = {
+				line = "<Leader>tlc",
+				block = "<Leader>tbc",
+			},
+			opleader = {
+				line = "<Leader>tl",
+				block = "<Leader>tb",
+			},
+			extra = {
+				above = "<Leader>aca",
+				below = "<Leader>acb",
+				eol = "<Leader>ace",
+			},
+		},
+	},
 }
