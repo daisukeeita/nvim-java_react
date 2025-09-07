1d73fc7f198da6ccfbf646397e66716cac4bc435
JAVV <vinas.johnandrei@gmail.com>
2025-09-08 03:08:55 +0800

    format

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,22 +1,22 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add          = { text = "┃" },
-        change       = { text = "┃" },
-        delete       = { text = "󰍵" },
-        topdelete    = { text = "󰍵" },
-        changedelete = { text = "┃" },
-        untracked    = { text = "┆" },
-      },
-      sign_priority = 6,
-      numhl = true,
-      current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 500,
-        virt_text_pos = "eol",
-      }
-    }
-  }
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "󰍵" },
+				topdelete = { text = "󰍵" },
+				changedelete = { text = "┃" },
+				untracked = { text = "┆" },
+			},
+			sign_priority = 6,
+			numhl = true,
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 500,
+				virt_text_pos = "eol",
+			},
+		},
+	},
 }
