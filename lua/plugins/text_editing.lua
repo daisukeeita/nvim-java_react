5650dc904e21765f82c6980d6307d46042deb1c6
JAVV <vinas.johnandrei@gmail.com>
2025-09-08 03:09:07 +0800

    removed comment nvim

--- lua/plugins/text_editing.lua
+++ lua/plugins/text_editing.lua
@@ -9,25 +9,9 @@ return {
 		version = "*",
 		opts = {
 			mappings = {
-				toggle = "<Leader>tsj",
-			},
-		},
-	},
-	{
-		"numToStr/Comment.nvim",
-		opts = {
-			toggler = {
-				line = "<Leader>tlc",
-				block = "<Leader>tbc",
-			},
-			opleader = {
-				line = "<Leader>tl",
-				block = "<Leader>tb",
-			},
-			extra = {
-				above = "<Leader>aca",
-				below = "<Leader>acb",
-				eol = "<Leader>ace",
+				toggle = "",
+				split = "",
+				join = "",
 			},
 		},
 	},
