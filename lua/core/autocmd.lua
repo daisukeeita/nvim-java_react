69d4b3d2a69346fd39d53ebc0f4972bf8d4e8587
JAVV <vinas.johnandrei@gmail.com>
2025-09-08 01:10:17 +0800

    closing float diagnostic on cursor movement

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -7,7 +7,6 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 
 -- Auto Run Formatters on Save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
 	callback = function(args)
 		require("conform").format({ bufnr = args.buf })
 	end,
@@ -18,6 +17,20 @@ vim.api.nvim_create_autocmd({ "CursorHold" }, {
 	group = vim.api.nvim_create_augroup("float_diagnostic_cursor", { clear = true }),
 	callback = function()
 		vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+	end,
+})
+
+-- Close diagnostic float on movement or buffer/tab change
+vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+	group = vim.api.nvim_create_augroup("close_float_diagnostic", { clear = true }),
+	callback = function()
+		-- Close only if a floating window exists
+		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			local config = vim.api.nvim_win_get_config(win)
+			if config.relative == "win" then -- floating window
+				vim.api.nvim_win_close(win, false)
+			end
+		end
 	end,
 })
 
