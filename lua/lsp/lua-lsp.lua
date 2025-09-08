1c7cb6c0b9e526926c8f8d55acae249444fc3931
JAVV <vinas.johnandrei@gmail.com>
2025-09-08 11:01:40 +0800

    added navic functionality in lua lsp

--- lua/lsp/lua-lsp.lua
+++ lua/lsp/lua-lsp.lua
@@ -1,50 +1,43 @@
-vim.lsp.config('lua_ls', {
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if
-        path ~= vim.fn.stdpath('config')
-        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-      then
-        return
-      end
-    end
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local navic = require("nvim-navic")
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most
-        -- likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Tell the language server how to find Lua modules same way as Neovim
-        -- (see `:h lua-module-load`)
-        path = {
-          'lua/?.lua',
-          'lua/?/init.lua',
-        },
-      },
-      -- Make the server aware of Neovim runtime files
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
-          -- Depending on the usage, you might want to add additional paths
-          -- here.
-          -- '${3rd}/luv/library'
-          -- '${3rd}/busted/library'
-        }
-        -- Or pull in all of 'runtimepath'.
-        -- NOTE: this is a lot slower and will cause issues when working on
-        -- your own configuration.
-        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-        -- library = {
-        --   vim.api.nvim_get_runtime_file('', true),
-        -- }
-      }
-    })
-  end,
-  settings = {
-    Lua = {}
-  }
+local on_attach = function(client, bufnr)
+	if client.server_capabilities.documentSymbolProvider then
+		navic.attach(client, bufnr)
+	end
+	if client.server_capabilities.inlayHintProvider then
+		vim.lsp.inlay_hint.enable(true, { bufnr })
+	end
+end
+
+vim.lsp.config("lua_ls", {
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			workspace = {
+				checkThirdParty = false,
+				library = { vim.env.VIMRUNTIME, vim.api.nvim_get_runtime_file("", true) },
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			telemetry = { enable = false },
+			completion = {
+				callSnippet = "Both",
+				keywordSnippet = "Both",
+				workspaceWord = true,
+			},
+			hint = {
+				enable = true,
+				paramType = false,
+				semicolon = false,
+				setType = true,
+				arrayIndex = false,
+			},
+		},
+	},
 })
 
-vim.lsp.enable('lua_ls')
+vim.lsp.enable("lua_ls")
